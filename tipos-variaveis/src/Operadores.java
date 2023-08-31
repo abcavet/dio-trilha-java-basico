c2cadba2600da4426d17b429d26510be906357ea
abcavet <abcavet@gmail.com>
2023-08-31 19:25:06 -0300

    operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,3 +1,11 @@
 public class Operadores {
     
+    public static void main(String [] args) {
+        
+        boolean variavel = true;
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+    }
 }
